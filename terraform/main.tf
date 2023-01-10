e527bfabdd79a53b292d75b7c2fd6a3eb9071835
pat-lafferty <patrick.lafferty@slalom.com>
2023-01-10 10:14:03 -0500

    template deployment fixes

--- terraform/main.tf
+++ terraform/main.tf
@@ -41,9 +41,40 @@ resource "azurerm_resource_group_template_deployment" "this" {
   name = "${local.name_prefix}-deployment"
   resource_group_name = azurerm_resource_group.this.name
   deployment_mode = "Incremental"
-  parameters_content = merge({
-    "workflowName" = azurerm_logic_app_workflow.this.name
-    "location"     = azurerm_resource_group.this.location
-  }, local.arm_params)
+  parameters_content = jsonencode({
+    "logicAppName" = {
+      value = local.arm_params["logicAppName"]
+    }
+    "When_a_message_is_received_in_a_queue_(auto-complete)Frequency" = {
+      value = local.arm_params["Frequency"]
+    }
+    "When_a_message_is_received_in_a_queue_(auto-complete)Interval" = {
+      value = local.arm_params["Interval"]
+    }
+    "owner_Tag" = {
+      value = local.common_tags["Owner"]
+    }
+    "office365_name" = {
+      value = local.arm_params["office365_name"]
+    }
+    "office365_displayName" = {
+      value = local.arm_params["office365_name"]
+    }
+    "servicebus_name" = {
+      value = local.arm_params["servicebus_name"]
+    }
+    "servicebus_displayName" = {
+      value = local.arm_params["servicebus_displayName"]
+    }
+    "servicebus_namespace_name" = {
+      value = local.arm_params["servicebus_namespace_name"]
+    }
+    "servicebus_resourceGroupName" = {
+      value = local.arm_params["servicebus_resourceGroupName"]
+    }
+    "servicebus_accessKey_name" = {
+      value = local.arm_params["servicebus_accessKey_name"]
+    }
+  })
   template_content = data.template_file.workflow.template
 }
